08a1afae9edb738fb0bfa4691fed9fdd3fca7c7f
Avishaan <avishaan@codeHatcher.com>
2015-06-10 01:43:06 -0700

    custom color for label

--- questionApp/UIButtonNext.swift
+++ questionApp/UIButtonNext.swift
@@ -9,12 +9,15 @@
 import UIKit
 
 let omnesFont = UIFont(name: "Omnes", size: 30)
+let orange = UIColor(red: 243/255, green: 108/255, blue: 43/255, alpha: 1.0)
 
 class UIButtonNext: UIButton {
 
     override func drawRect(rect: CGRect) {
       self.setTitle("save profile", forState: UIControlState.Normal)
       self.titleLabel!.font = omnesFont
+      self.setTitleColor(orange, forState: UIControlState.Normal)
+      //self.titleLabel!.textColor = UIColor(red: 243.0/255.0, green: 108.0/255.0, blue: 43.0/255.0, alpha: 1.0)
       // self.setImage(UIImage(named: "startButton"), forState: UIControlState.Normal)
       self.setBackgroundImage(UIImage(named: "baseButton"), forState: UIControlState.Normal)
     }
