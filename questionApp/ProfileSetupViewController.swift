53f2028a0d4d94354d187c1039945aa8fbda33bd
Avishaan <avishaan@codeHatcher.com>
2015-06-10 02:51:46 -0700

    add segmented controller for gender

--- questionApp/ProfileSetupViewController.swift
+++ questionApp/ProfileSetupViewController.swift
@@ -9,27 +9,35 @@
 import UIKit
 
 class ProfileSetupViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  
+  @IBOutlet weak var genderControl: UISegmentedControl!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    // Do any additional setup after loading the view.
+    // set font on segmented controller
+    var attr = NSDictionary(object: UIFont(name: "Omnes-Medium", size: 13)!, forKey: NSFontAttributeName)
+    genderControl.setTitleTextAttributes(attr as [NSObject : AnyObject], forState: .Normal)
+    // give a little bit of tint
+    genderControl.tintColor = UIColor.grayColor()
+    // TODO: change color based on which gender is picked
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+  /*
+  // MARK: - Navigation
+  
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+  // Get the new view controller using segue.destinationViewController.
+  // Pass the selected object to the new view controller.
+  }
+  */
+  
 }
