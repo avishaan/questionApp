da575388ca7835d45d3680b2ae2f1925a504df75
Avishaan <avishaan@codeHatcher.com>
2015-06-10 17:34:35 -0700

    section title button revised and saved profile controller added

--- questionApp/BNSectionLabel.swift
+++ questionApp/BNSectionLabel.swift
@@ -11,10 +11,34 @@ import UIKit
 class BNSectionLabel: UILabel {
 
     override func drawRect(rect: CGRect) {
+      // make sure it is wide enough to prevent it from being cut off
+      self.layer.frame = CGRect(
+        x: 53,
+        y: 20,
+        width: 215,
+        height: 38
+      )
+//      self.bounds = CGRect(
+//        x: 0,
+//        y: 0,
+//        width: 215,
+//        height: 38
+//      )
+//      self.frame = CGRect(
+//        x: self.frame.origin.x,
+//        y: self.frame.origin.y,
+//        width: self.frame.size.width,
+//        height: self.frame.size.height
+//      )
+//      self.center = CGPoint(x: 160.5, y: 39)
+//      
+//      self.layer.borderColor = UIColor.blackColor().CGColor
+//      self.layer.borderWidth = 5.0
+//      
+//      //self.text = "Test"
+//      //self.font = omnesFontMed
       self.font = UIFont(name: kOmnesFontMedium, size: 29)
       self.textAlignment = .Center
-      //self.text = "Test"
-      //self.font = omnesFontMed
       self.textColor = UIColor.whiteColor()
       // make the actual drawings
       super.drawRect(rect)
