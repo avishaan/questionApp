59e61def83be30e317a419a4b0cd68d66e7e16f8
Avishaan <avishaan@codeHatcher.com>
2015-06-10 20:19:42 -0700

    add attributed string text with correct colors

--- questionApp/SavedProfileViewController.swift
+++ questionApp/SavedProfileViewController.swift
@@ -9,27 +9,47 @@
 import UIKit
 
 class SavedProfileViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  
+  @IBOutlet weak var nameLabel: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    // Do any additional setup after loading the view.
+    
+    var attrLabel = [NSFontAttributeName: UIFont(name: kOmnesFontSemiBold, size: 19)!]
+    var attrValue = [NSFontAttributeName: UIFont(name: kOmnesFontMedium, size: 19)!]
+    var myString = "Name: "
+    var myString2 = "Molly Smith"
+    var combinedString = myString + myString2
+    var myMutableString = NSMutableAttributedString(string: combinedString, attributes: attrLabel)
+    var range = combinedString.rangeOfString(myString2)
+    //Add more attributes here
+//    myMutableString.addAttributes(attrValue, range: combinedString.rangeOfString(myString2))
+//    myMutableString.addAttributes(attrValue as AnyObject, range: range)
+    myMutableString.addAttribute(NSFontAttributeName, value: UIFont(name: kOmnesFontMedium, size: 20)!, range: NSMakeRange(count(myString), count(myString2)))
+    myMutableString.addAttribute(NSForegroundColorAttributeName, value: UIColor.redColor(), range: NSMakeRange(count(myString), count(myString2)))
+//    myMutableString.addAttribute(attrValue, range: combinedString.rangeOfString(myString2))
+    
+    //Apply to the label
+    nameLabel.attributedText = myMutableString
+    
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+  /*
+  // MARK: - Navigation
+  
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+  // Get the new view controller using segue.destinationViewController.
+  // Pass the selected object to the new view controller.
+  }
+  */
+  
 }
