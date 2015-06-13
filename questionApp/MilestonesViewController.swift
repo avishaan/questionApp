f9d9a882e4510c8ea640d70dabdc959bfd911037
Avishaan <avishaan@codeHatcher.com>
2015-06-12 22:39:16 -0700

    round image view for milestone controller

--- questionApp/MilestonesViewController.swift
+++ questionApp/MilestonesViewController.swift
@@ -9,27 +9,35 @@
 import UIKit
 
 class MilestonesViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  
+  @IBOutlet weak var babyImageView: UIImageView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    // Do any additional setup after loading the view.
+    // setup image as a circle
+    babyImageView.layer.cornerRadius = babyImageView.frame.size.width/4
+    babyImageView.clipsToBounds = true
+    babyImageView.layer.borderWidth = 5.0
+    babyImageView.layer.borderColor = kBlue.CGColor
+    
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+  /*
+  // MARK: - Navigation
+  
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+  // Get the new view controller using segue.destinationViewController.
+  // Pass the selected object to the new view controller.
+  }
+  */
+  
 }
