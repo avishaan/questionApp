618f80fe863859bb22b656e57c1744c6e020e310
Avishaan <avishaan@codeHatcher.com>
2015-06-29 02:03:47 -0700

    finish time to test for pupil response

--- questionApp/PupilResponseTimeToTestViewController.swift
+++ questionApp/PupilResponseTimeToTestViewController.swift
@@ -10,13 +10,40 @@ import UIKit
 
 class PupilResponseTimeToTestViewController: UIViewController {
   
+  @IBOutlet weak var step1Label: UILabel!
+  @IBOutlet weak var step2Label: UILabel!
+  @IBOutlet weak var step3Label: UILabel!
+  @IBOutlet weak var step4Label: UILabel!
+  
   @IBOutlet weak var labelBackground: UILabel!
+  
+  
+  let orangeAtrributes = [NSForegroundColorAttributeName: kOrange, NSFontAttributeName: UIFont(name: kOmnesFontSemiBold, size: 22)!]
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
     // Do any additional setup after loading the view.
     labelBackground.layer.cornerRadius = 7
     labelBackground.layer.masksToBounds = true
+    
+    // first character of each label should be orange
+    var step1AttributedString = NSMutableAttributedString(string: step1Label.text!)
+    step1AttributedString.addAttributes(orangeAtrributes, range: NSMakeRange(0, 2))
+    step1Label.attributedText = step1AttributedString
+    
+    var step2AttributedString = NSMutableAttributedString(string: step2Label.text!)
+    step2AttributedString.addAttributes(orangeAtrributes, range: NSMakeRange(0, 2))
+    step2Label.attributedText = step2AttributedString
+    
+    var step3AttributedString = NSMutableAttributedString(string: step3Label.text!)
+    step3AttributedString.addAttributes(orangeAtrributes, range: NSMakeRange(0, 2))
+    step3Label.attributedText = step3AttributedString
+    
+    var step4AttributedString = NSMutableAttributedString(string: step4Label.text!)
+    step4AttributedString.addAttributes(orangeAtrributes, range: NSMakeRange(0, 2))
+    step4Label.attributedText = step4AttributedString
+    
   }
   
   override func didReceiveMemoryWarning() {
