a84e885a0abd83bc5fabc8fe79a0d860a939c226
Avishaan <avishaan@codeHatcher.com>
2015-06-28 23:00:56 -0700

    finalize test overview for pupil response

---
+++ questionApp/PupilResponseTestOverviewViewController.swift
@@ -0,0 +1,60 @@
+//
+//  PupilResponseTestOverviewViewController.swift
+//  questionApp
+//
+//  Created by Brown Magic on 6/28/15.
+//  Copyright (c) 2015 codeHatcher. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class PupilResponseTestOverviewViewController: UIViewController {
+  
+  @IBOutlet weak var previewButton: UIButton!
+  @IBOutlet weak var player: UIView!
+  
+  var playerVC:AVPlayerViewController!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    if segue.identifier == "embeddedVideoSegue" {
+      // set the playerVC as out destination
+      playerVC = segue.destinationViewController as!
+      AVPlayerViewController
+      let path = NSBundle.mainBundle().pathForResource("crawl", ofType: "mp4")
+      let url = NSURL.fileURLWithPath(path!)
+      // let url = NSURL(string: "crawl.mp4") // for remote locations
+      
+      // hide player controls
+      playerVC.showsPlaybackControls = false
+      playerVC.hidesBottomBarWhenPushed = true
+      playerVC.videoGravity = AVLayerVideoGravityResizeAspectFill
+      
+      playerVC.player = AVPlayer(URL: url)
+      // we start off paused, then we will play once the button is hit
+      playerVC.player.pause()
+    }
+    
+  }
+  @IBAction func onPreviewButtonTap(sender: AnyObject) {
+    // we know the sender is a button, cast accordingly
+    var button = sender as! UIButton
+    // hide this button
+    button.hidden = true
+    // play the video
+    playerVC.player.play()
+  }
+  
+}
