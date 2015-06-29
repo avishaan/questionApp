5c212c71f2afbe9d9267c8f0aa89b3c16cdf5595
Avishaan <avishaan@codeHatcher.com>
2015-06-28 23:27:55 -0700

    finish whatWillYouNeed view controller

--- questionApp/PupilResponseWhatWillYouNeedViewController.swift
+++ questionApp/PupilResponseWhatWillYouNeedViewController.swift
@@ -9,27 +9,43 @@
 import UIKit
 
 class PupilResponseWhatWillYouNeedViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  
+  @IBOutlet weak var testPreparationLabel: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    // Do any additional setup after loading the view.
+    
+    // set up attributes for both part of pupil response description label
+    let string = "You can use the light on your phone (if the baby will keep his hands off it!) or a pen light. Not too bright though."
+    var attributedString = NSMutableAttributedString(string: string)
+    
+    let baseAttributes = [NSForegroundColorAttributeName: kGrey, NSFontAttributeName: UIFont(name: kOmnesFontMedium, size: 22)!]
+    let firstAttributes = [NSForegroundColorAttributeName: kGrey, NSFontAttributeName: UIFont(name: kOmnesFontSemiBold, size: 22)!]
+    let secondAttributes = [NSForegroundColorAttributeName: kGrey, NSFontAttributeName: UIFont(name: kOmnesFontMedium, size: 22)!]
+    
+    attributedString.addAttributes(baseAttributes, range: NSMakeRange(1, 16))
+    attributedString.addAttributes(firstAttributes, range: NSMakeRange(16, 19))
+    attributedString.addAttributes(secondAttributes, range: NSMakeRange(36, 80))
+    
+    testPreparationLabel.attributedText = attributedString
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+  /*
+  // MARK: - Navigation
+  
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+  // Get the new view controller using segue.destinationViewController.
+  // Pass the selected object to the new view controller.
+  }
+  */
+  
 }
