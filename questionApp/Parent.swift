9c643f91458660d3c5a5687e9df7ec2bc597f828
Avishaan <avishaan@codeHatcher.com>
2015-07-05 22:55:48 -0400

    add other properties for parent

--- questionApp/Parent.swift
+++ questionApp/Parent.swift
@@ -11,17 +11,33 @@ import Foundation
 class Parent {
   var fullName: String?
   var email: String?
+  var babyName: String?
+  var babyGender: String?
+  var babyBirthday: NSDate?
+  
+  let kFullName = "FullName"
+  let kEmail = "Email"
+  let kBabyName = "BabyName"
+  let kBabyGender = "BabyGender"
+  let kBabyBirthday = "BabyBirthday"
+  
   let store = NSUserDefaults.standardUserDefaults()
   
   init() {
     // first get info from the defaults incase they already exist
-    self.fullName = store.objectForKey("fullName") as? String
-    self.email = store.objectForKey("email") as? String
+    self.fullName = store.objectForKey(kFullName) as? String
+    self.email = store.objectForKey(kEmail) as? String
+    self.babyName = store.objectForKey(kBabyName) as? String
+    self.babyGender = store.objectForKey(kBabyGender) as? String
+    self.babyBirthday = store.objectForKey(kBabyBirthday) as? NSDate
   }
   
   func storeInfo() {
-    store.setObject(self.fullName, forKey: "fullName")
-    store.setObject(self.email, forKey: "email")
+    store.setObject(self.fullName, forKey: kFullName)
+    store.setObject(self.email, forKey: kEmail)
+    store.setObject(self.babyName, forKey: kBabyName)
+    store.setObject(self.babyGender, forKey: kBabyGender)
+    store.setObject(self.babyBirthday, forKey: kBabyBirthday)
   }
   
 }
