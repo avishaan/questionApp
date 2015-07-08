ea8cfc65c644e984856bc0d7d766f333426f7751
jbateman <johnbateman00@gmail.com>
2015-07-07 20:35:02 -0700

    LanguageCognitiveIntroViewController

---
+++ questionApp/LanguageCognitiveIntroViewController.swift
@@ -0,0 +1,54 @@
+//
+//  LanguageCognitiveIntroViewController.swift
+//  questionApp
+//
+//  Created by john bateman on 7/7/15.
+//  Copyright (c) 2015 codeHatcher. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class LanguageCognitiveIntroViewController: UIViewController {
+
+    @IBOutlet weak var previewButton: UIButton!
+    @IBOutlet weak var player: UIView!
+    
+    var playerVC:AVPlayerViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "LanguageCognitiveTutorialVideoSegue" {
+            // set the playerVC as the destination
+            playerVC = segue.destinationViewController as! AVPlayerViewController
+            let path = NSBundle.mainBundle().pathForResource("crawl", ofType: "mp4")
+            let url = NSURL.fileURLWithPath(path!)
+            // let url = NSURL(string: "crawl.mp4") // for remote locations
+            
+            // hide player controls
+            playerVC.showsPlaybackControls = false
+            playerVC.hidesBottomBarWhenPushed = true
+            playerVC.videoGravity = AVLayerVideoGravityResizeAspectFill
+            
+            playerVC.player = AVPlayer(URL: url)
+            // we start off paused, then we will play once the button is hit
+            playerVC.player.pause()
+        }
+    }
+    @IBAction func onPreviewButtonTap(button: UIButton) {
+        // hide this button
+        button.hidden = true
+        
+        // play the video
+        playerVC.player.play()
+    }
+}
