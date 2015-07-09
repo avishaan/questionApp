6e519f7437d8c28b88f6bb228b620351d06bd9a4
jbateman <johnbateman00@gmail.com>
2015-07-08 22:42:49 -0700

    ActivityReminderViewController text message reminder

--- questionApp/ActivityReminderViewController.swift
+++ questionApp/ActivityReminderViewController.swift
@@ -15,18 +15,26 @@ struct EmailConstants {
     static var subject = "Falling Toy Test Reminder"
     static var body = "babynoggin reminds you to try the Falling Toy test again in 2-4 weeks."
     static var emailBodyFormatted = "babynoggin reminds you to try the %@ test again in 2-4 weeks."
-    static var composerErrorTitle = "Unable to send email"
+    static var composerErrorTitle = "Unable to send the email"
     static var composerErrorMessage = "The device is not configured to send email. Please check that an e-mail account is configured in settings and retry."
 }
 
-class ActivityReminderViewController: UIViewController, MFMailComposeViewControllerDelegate {
+struct MessageConstants {
+    static var recipient = ""
+    static var subject = "Falling Toy Test Reminder"
+    static var body = "babynoggin reminds you to try the Falling Toy test again in 2-4 weeks."
+    static var messageBodyFormatted = "babynoggin reminds you to try the %@ test again in 2-4 weeks."
+    static var composerErrorTitle = "Unable to send the message"
+    static var composerErrorMessage = "The device is not configured to send text messages. Please check settings and retry."
+}
+
+class ActivityReminderViewController: UIViewController, MFMailComposeViewControllerDelegate, MFMessageComposeViewControllerDelegate {
 
     // The object that invokes this controller should set testName to something similar to "Falling Toy" to identify the test. It will be used in the subject and body of the reminder email.
     var testName: String? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        testName = "falling toy"
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,19 +46,73 @@ class ActivityReminderViewController: UIViewController, MFMailComposeViewControl
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
+    // TODO: consider moving MFMessageComposeViewController and MFMailComposeViewController code to their own classes to lighten the view controller.
+    
+    // Mark: text message
+    
     @IBAction func onTextMessageButtonTap(sender: AnyObject) {
-    }
-    
-    
-    @IBAction func onInAppNotificationTap(sender: AnyObject) {
+        
+        if MFMessageComposeViewController.canSendText() {
+            if let messageComposeViewController = setupMessageComposeController() {
+                self.presentViewController(messageComposeViewController, animated: true, completion: nil)
+            } else {
+                presentMessageErrorAlert()
+            }
+        } else {
+            presentMessageErrorAlert()
+        }
     }
 
+    func setupMessageComposeController() -> MFMessageComposeViewController? {
+        // configure composer
+        let messageComposeViewController:MFMessageComposeViewController? = MFMessageComposeViewController()
+        if let messageComposeViewController = messageComposeViewController {
+            messageComposeViewController.messageComposeDelegate = self
+            
+            // configure message recipient
+            let parent = Parent()
+            // Note: Parent currently does not hold a mobile number, so nothing to configure here yet.
+            // messageComposeViewController.recipients = parent.
+            
+            // configure message subject and body
+            if let testName = self.testName {
+                // subject
+                if MFMessageComposeViewController.canSendSubject() {
+                    messageComposeViewController.subject = testName + " test reminder"
+                }
+                // body
+                var bodyString = String(format: MessageConstants.messageBodyFormatted, testName)
+                messageComposeViewController.body = bodyString
+            } else {
+                // subject
+                if MFMessageComposeViewController.canSendSubject() {
+                    messageComposeViewController.subject = MessageConstants.subject
+                }
+                // body
+                messageComposeViewController.body = MessageConstants.body
+            }
+        }
+        
+        return messageComposeViewController
+    }
+
+    func presentMessageErrorAlert() {
+        let alert = UIAlertView(title: MessageConstants.composerErrorTitle, message: MessageConstants.composerErrorMessage, delegate: self, cancelButtonTitle: "OK")
+        alert.show()
+    }
+    
+    // MFMessageComposeViewControllerDelegate. Called when the message is completed. Dismiss the message controller.
+    func messageComposeViewController(controller: MFMessageComposeViewController!, didFinishWithResult result: MessageComposeResult) {
+        controller.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    
     // MARK: e-mail
     
     @IBAction func onEmailButtonTap(sender: AnyObject) {
         if MFMailComposeViewController.canSendMail() {
-            if let mailComposeController = setupMailComposeController() {
-                self.presentViewController(mailComposeController, animated: true, completion: nil)
+            if let mailComposeViewController = setupMailComposeController() {
+                self.presentViewController(mailComposeViewController, animated: true, completion: nil)
             } else {
                 presentMailErrorAlert()
             }
@@ -61,30 +123,30 @@ class ActivityReminderViewController: UIViewController, MFMailComposeViewControl
     
     func setupMailComposeController() -> MFMailComposeViewController? {
         // configure composer
-        let mailComposeController:MFMailComposeViewController? = MFMailComposeViewController()
-        if let mailComposeController = mailComposeController {
-            mailComposeController.mailComposeDelegate = self
+        let mailComposeViewController:MFMailComposeViewController? = MFMailComposeViewController()
+        if let mailComposeViewController = mailComposeViewController {
+            mailComposeViewController.mailComposeDelegate = self
             
             // configure message recipient
             let parent = Parent()
             if let email = parent.email {
-                mailComposeController.setToRecipients([email])
+                mailComposeViewController.setToRecipients([email])
             } else {
-                mailComposeController.setToRecipients([EmailConstants.recipient])
+                mailComposeViewController.setToRecipients([EmailConstants.recipient])
             }
             
             // configure message subject and body
             if let testName = self.testName {
-                mailComposeController.setSubject(testName + " test reminder")
+                mailComposeViewController.setSubject(testName + " test reminder")
                 var bodyString = String(format: EmailConstants.emailBodyFormatted, testName)
-                mailComposeController.setMessageBody(bodyString, isHTML: false)
+                mailComposeViewController.setMessageBody(bodyString, isHTML: false)
             } else {
-                mailComposeController.setSubject(EmailConstants.subject)
-                mailComposeController.setMessageBody(EmailConstants.body, isHTML: false)
+                mailComposeViewController.setSubject(EmailConstants.subject)
+                mailComposeViewController.setMessageBody(EmailConstants.body, isHTML: false)
             }
         }
         
-        return mailComposeController
+        return mailComposeViewController
     }
     
     func presentMailErrorAlert() {
@@ -92,9 +154,16 @@ class ActivityReminderViewController: UIViewController, MFMailComposeViewControl
         alert.show()
     }
     
-    // MARK: MFMailComposeViewControllerDelegate Method
+    // MFMailComposeViewControllerDelegate Method. Called when the email is completed. Dismiss the mail controller.
     func mailComposeController(controller: MFMailComposeViewController!, didFinishWithResult result: MFMailComposeResult, error: NSError!) {
         controller.dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    
+    // MARK: local notification
+    
+    @IBAction func onInAppNotificationTap(sender: AnyObject) {
+    }
+
 
 }
