9f3523bb60bdb18af851c468c7403913366bbf68
Avishaan <avishaan@codeHatcher.com>
2015-07-13 13:34:53 -0400

    horizontal bar chart not available, need to use with limits Revert "used combined bar chart"
    This reverts commit 94ca69b5531df99c07df3d575d8cadd712dc3232.

--- questionApp/ViewController.swift
+++ questionApp/ViewController.swift
@@ -13,7 +13,7 @@ import Charts
 class ViewController: BNUIViewController {
 
   @IBOutlet weak var subView: UIView!
-  @IBOutlet weak var chartView: CombinedChartView!
+  @IBOutlet weak var barChartView: HorizontalBarChartView!
   
   var months: [String]?
   
@@ -41,24 +41,21 @@ class ViewController: BNUIViewController {
   }
   
   func setChart(dataPoints: [String], values: [Double]) {
-    chartView.noDataText = "No data to show you"
-    
-    var dataEntries:[BarChartDataEntry] = []
+    barChartView.noDataText = "No data to show you"
+    var dataEntries: [BarChartDataEntry] = []
     
     for i in 0..<dataPoints.count {
       let dataEntry = BarChartDataEntry(value: values[i], xIndex: i)
       dataEntries.append(dataEntry)
     }
     
-    let barChartDataSet = BarChartDataSet(yVals: dataEntries, label: "Units Sold")
-    let barChartData = BarChartData(xVals: months, dataSet: barChartDataSet)
-    var data: CombinedChartData = CombinedChartData(xVals: months, dataSet: barChartDataSet)
-    data.barData = barChartData
-    chartView.data = data
+    let chartDataSet = BarChartDataSet(yVals: dataEntries, label: "Units Sold")
+    let chartData = BarChartData(xVals: months, dataSet: chartDataSet)
+    barChartView.data = chartData
     
-    chartView.descriptionText = ""
-    chartView.legend.enabled = false
-    chartView.xAxis.enabled = false
+    barChartView.descriptionText = ""
+    barChartView.legend.enabled = false
+    barChartView.xAxis.enabled = false
   }
   
 
