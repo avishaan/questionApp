8cf5cbb7ef1d47deee8803b480ee26263b54b5fa
Avishaan <avishaan@codeHatcher.com>
2015-07-13 12:26:21 -0400

    placeholder barchart

--- questionApp/ViewController.swift
+++ questionApp/ViewController.swift
@@ -13,11 +13,18 @@ import Charts
 class ViewController: BNUIViewController {
 
   @IBOutlet weak var subView: UIView!
+  @IBOutlet weak var barChartView: BarChartView!
+  
+  var months: [String]?
   
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     
+    months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
+    
+    setChart(months!, values: unitsSold)
     
     //check all fonts
     for family in UIFont.familyNames() {
@@ -31,6 +38,21 @@ class ViewController: BNUIViewController {
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
+  }
+  
+  func setChart(dataPoints: [String], values: [Double]) {
+    barChartView.noDataText = "No data to show you"
+    var dataEntries: [BarChartDataEntry] = []
+    
+    for i in 0..<dataPoints.count {
+      let dataEntry = BarChartDataEntry(value: values[i], xIndex: i)
+      dataEntries.append(dataEntry)
+    }
+    
+    let chartDataSet = BarChartDataSet(yVals: dataEntries, label: "Units Sold")
+    let chartData = BarChartData(xVals: months, dataSet: chartDataSet)
+    barChartView.data = chartData
+    
   }
   
 
