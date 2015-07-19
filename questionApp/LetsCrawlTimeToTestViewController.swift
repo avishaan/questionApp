f2a8e75a0a3bedd5b83424bc4c5137349a495e19
Avishaan <avishaan@codeHatcher.com>
2015-07-18 21:12:27 -0400

    remove unecessary flashlight button #23

--- questionApp/LetsCrawlTimeToTestViewController.swift
+++ questionApp/LetsCrawlTimeToTestViewController.swift
@@ -22,8 +22,6 @@ class LetsCrawlTimeToTestViewController: UIViewController {
 
     let orangeAtrributes = [NSForegroundColorAttributeName: kOrange, NSFontAttributeName: UIFont(name: kOmnesFontSemiBold, size: 22)!]
 
-    let device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -39,47 +37,10 @@ class LetsCrawlTimeToTestViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func onFlashlightButtonTap(sender: BNButtonNext) {
-        // does the iPhone have a flashlight?
-        if (isFlashlightAvailable()) {
-            device.lockForConfiguration(nil)
-            // check if flashlight is on for toggle
-            if (device.torchMode == AVCaptureTorchMode.On) {
-                device.torchMode = AVCaptureTorchMode.Off
-            } else {
-                device.setTorchModeOnWithLevel(1.0, error: nil)
-            }
-            device.unlockForConfiguration()
-        }
-    }
-
-    func turnOffLightWithLock() {
-        // does the iPhone have a flashlight?
-        if (isFlashlightAvailable()) {
-            //check if light is even on first
-            if (device.torchMode == AVCaptureTorchMode.On) {
-                // if on, turn off
-                device.lockForConfiguration(nil)
-                device.torchMode = AVCaptureTorchMode.Off
-                device.unlockForConfiguration()
-            }
-        }
-    }
-    
-    func isFlashlightAvailable() -> Bool {
-        if (device != nil && device.hasTorch) {
-            return true
-        } else {
-            return false
-        }
-    }
-    
     @IBAction func onNextButtonTap(sender: BNButtonNext) {
-        turnOffLightWithLock()
     }
     
     @IBAction func onBackButtonTap(sender: AnyObject) {
-        turnOffLightWithLock()
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
