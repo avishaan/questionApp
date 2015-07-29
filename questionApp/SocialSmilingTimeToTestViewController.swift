23e8bcd61b951aabb97a1af0cdd316b4e3da0a7a
Daniel Hsu <danieljhsu@gmail.com>
2015-07-28 17:52:49 -0700

    set up scaffolding for Social Smiling test #30

---
+++ questionApp/SocialSmilingTimeToTestViewController.swift
@@ -0,0 +1,109 @@
+//
+//  PointFollowingTimeToTestViewController.swift
+//  questionApp
+//
+//  Created by john bateman on 7/13/15.
+//  Copyright (c) 2015 codeHatcher. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PointFollowingTimeToTestViewController: UIViewController {
+
+    @IBOutlet weak var step1Label: UILabel!
+    
+    @IBOutlet weak var step2Label: UILabel!
+    @IBOutlet weak var step3Label: UILabel!
+    @IBOutlet weak var step4Label: UILabel!
+    @IBOutlet weak var step5Label: UILabel!
+    
+    @IBOutlet weak var labelBackground: UILabel!
+    
+    let orangeAtrributes = [NSForegroundColorAttributeName: kOrange, NSFontAttributeName: UIFont(name: kOmnesFontSemiBold, size: 22)!]
+    
+    let device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // setup rounded corners on label
+        labelBackground.layer.cornerRadius = 7
+        labelBackground.layer.masksToBounds = true
+        applyTextAttributesToLabel()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onFlashlightButtonTap(sender: BNButtonNext) {
+        // does the iPhone have a flashlight?
+        if (isFlashlightAvailable()) {
+            device.lockForConfiguration(nil)
+            // check if flashlight is on for toggle
+            if (device.torchMode == AVCaptureTorchMode.On) {
+                device.torchMode = AVCaptureTorchMode.Off
+            } else {
+                device.setTorchModeOnWithLevel(1.0, error: nil)
+            }
+            device.unlockForConfiguration()
+        }
+    }
+    
+    func turnOffLightWithLock() {
+        // does the iPhone have a flashlight?
+        if (isFlashlightAvailable()) {
+            //check if light is even on first
+            if (device.torchMode == AVCaptureTorchMode.On) {
+                // if on, turn off
+                device.lockForConfiguration(nil)
+                device.torchMode = AVCaptureTorchMode.Off
+                device.unlockForConfiguration()
+            }
+        }
+    }
+    
+    func isFlashlightAvailable() -> Bool {
+        if (device != nil && device.hasTorch) {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    @IBAction func onNextButtonTap(sender: BNButtonNext) {
+        turnOffLightWithLock()
+    }
+
+    @IBAction func onBackButtonTap(sender: AnyObject) {
+        turnOffLightWithLock()
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // Helper function formats text attributes for multiple substrings in label.
+    func applyTextAttributesToLabel() {
+        
+        // first character of each label should be orange
+        var step1AttributedString = NSMutableAttributedString(string: step1Label.text!)
+        step1AttributedString.addAttributes(orangeAtrributes, range: NSMakeRange(0, 2))
+        step1Label.attributedText = step1AttributedString
+        
+        var step2AttributedString = NSMutableAttributedString(string: step2Label.text!)
+        step2AttributedString.addAttributes(orangeAtrributes, range: NSMakeRange(0, 2))
+        step2Label.attributedText = step2AttributedString
+        
+        var step3AttributedString = NSMutableAttributedString(string: step3Label.text!)
+        step3AttributedString.addAttributes(orangeAtrributes, range: NSMakeRange(0, 2))
+        step3Label.attributedText = step3AttributedString
+        
+        var step4AttributedString = NSMutableAttributedString(string: step4Label.text!)
+        step4AttributedString.addAttributes(orangeAtrributes, range: NSMakeRange(0, 2))
+        step4Label.attributedText = step4AttributedString
+        
+        var step5AttributedString = NSMutableAttributedString(string: step5Label.text!)
+        step5AttributedString.addAttributes(orangeAtrributes, range: NSMakeRange(0, 2))
+        step5Label.attributedText = step5AttributedString
+    }
+
+}
---
+++ questionApp/SocialSmilingWhatWillYouNeedViewControllerswift.swift
@@ -0,0 +1,55 @@
+//
+//  PointFollowingWhatWillYouNeedViewController.swift
+//  questionApp
+//
+//  Created by john bateman on 7/13/15.
+//  Copyright (c) 2015 codeHatcher. All rights reserved.
+//
+
+import UIKit
+
+class PointFollowingWhatWillYouNeedViewController: UIViewController {
+
+    @IBOutlet weak var testPreparationLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        applyTextAttributesToLabel()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onNextStepButtonTap(sender: AnyObject) {
+        let dontShowIsBabyReadyVC = NSUserDefaults.standardUserDefaults().boolForKey("dontShowIsBabyReady")
+        if dontShowIsBabyReadyVC == true {
+            performSegueWithIdentifier("pointFollowingTimeToTestSegueID", sender: self)
+        } else {
+            performSegueWithIdentifier("pointFollowingIsBabyReadySegueID", sender: self)
+        }
+    }
+    
+    @IBAction func onBackButtonTap(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    // Helper function formats text attributes for multiple substrings in label.
+    func applyTextAttributesToLabel() {
+        
+        let string = "You can use any object that will attract and hold your baby's attention."
+
+        var attributedString = NSMutableAttributedString(string: string)
+        
+        let baseAttributes = [NSForegroundColorAttributeName: kGrey, NSFontAttributeName: UIFont(name: kOmnesFontMedium, size: 22)!]
+        let firstAttributes = [NSForegroundColorAttributeName: kGrey, NSFontAttributeName: UIFont(name: kOmnesFontSemiBold, size: 22)!]
+        let secondAttributes = [NSForegroundColorAttributeName: kGrey, NSFontAttributeName: UIFont(name: kOmnesFontMedium, size: 22)!]
+        
+        attributedString.addAttributes(baseAttributes, range: NSMakeRange(0, 33))
+        attributedString.addAttributes(firstAttributes, range: NSMakeRange(32, 17))
+        attributedString.addAttributes(secondAttributes, range: NSMakeRange(49, 23))
+        
+        testPreparationLabel.attributedText = attributedString
+    }
+}
