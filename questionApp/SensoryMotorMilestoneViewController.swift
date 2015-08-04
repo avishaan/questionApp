429bbf7a622a4ecd6cc9c303dada0cd70c730911
Avishaan <avishaan@codeHatcher.com>
2015-08-04 01:04:50 -0700

    fix two targets when showing new VC #53

--- questionApp/SensoryMotorMilestoneViewController.swift
+++ questionApp/SensoryMotorMilestoneViewController.swift
@@ -9,35 +9,35 @@
 import UIKit
 
 class SensoryMotorMilestoneViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    @IBAction func onCrossingEyesButtonTap(sender: AnyObject) {
-        var storyboard = UIStoryboard (name: "CrossingEyes", bundle: nil)
-        var controller: WhyIsCrossingEyesViewController = storyboard.instantiateViewControllerWithIdentifier("WhyIsCrossingEyesStoryboardID") as! WhyIsCrossingEyesViewController
-        self.presentViewController(controller, animated: true, completion: nil);
-    }
-    
-    @IBAction func onHearingButtonTap(sender: AnyObject) {
-        var storyboard = UIStoryboard (name: "Hearing", bundle: nil)
-        var controller: WhyIsHearingViewController = storyboard.instantiateViewControllerWithIdentifier("WhyIsHearingStoryboardID") as! WhyIsHearingViewController
-        self.presentViewController(controller, animated: true, completion: nil);
-    }
-    
-    @IBAction func onSymmetryButtonTap(sender: AnyObject) {
-        var storyboard = UIStoryboard (name: "Symmetry", bundle: nil)
-        var controller: WhyIsSymmetryViewController = storyboard.instantiateViewControllerWithIdentifier("WhyIsSymmetryStoryboardID") as! WhyIsSymmetryViewController
-        self.presentViewController(controller, animated: true, completion: nil);
-    }
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  @IBAction func onCrossingEyesButtonTap(sender: AnyObject) {
+    var storyboard = UIStoryboard (name: "CrossingEyes", bundle: nil)
+    var controller: WhyIsCrossingEyesViewController = storyboard.instantiateViewControllerWithIdentifier("WhyIsCrossingEyesStoryboardID") as! WhyIsCrossingEyesViewController
+    self.presentViewController(controller, animated: true, completion: nil);
+  }
+  
+  @IBAction func onHearingButtonTap(sender: AnyObject) {
+    var storyboard = UIStoryboard (name: "Hearing", bundle: nil)
+    var controller: WhyIsHearingViewController = storyboard.instantiateViewControllerWithIdentifier("WhyIsHearingStoryboardID") as! WhyIsHearingViewController
+    self.presentViewController(controller, animated: true, completion: nil);
+  }
+  
+  @IBAction func onSymmetryButtonTap(sender: AnyObject) {
+    var storyboard = UIStoryboard (name: "Symmetry", bundle: nil)
+    var controller: WhyIsSymmetryViewController = storyboard.instantiateViewControllerWithIdentifier("WhyIsSymmetryStoryboardID") as! WhyIsSymmetryViewController
+    self.presentViewController(controller, animated: true, completion: nil);
+  }
   @IBAction func onPincerButtonTap(sender: AnyObject) {
     var storyboard = UIStoryboard (name: "Pincer", bundle: nil)
     var controller: WhyIsPincerViewController = storyboard.instantiateViewControllerWithIdentifier("WhyIsPincerStoryboardID") as! WhyIsPincerViewController
