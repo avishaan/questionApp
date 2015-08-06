b16068a6da7a6e2bb9628065e2088c0b8b49f46f
Daniel Hsu <danieljhsu@gmail.com>
2015-08-05 21:58:38 -0700

    set up scaffolding for unassisted sitting test #54

---
+++ questionApp/UnassistedSittingGoodOutcomeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PartiallyCoveredToyGoodOutcomeViewController.swift
+//  questionApp
+//
+//  Created by Daniel Hsu on 7/30/15.
+//  Copyright (c) 2015 codeHatcher. All rights reserved.
+//
+
+import UIKit
+
+class PartiallyCoveredToyGoodOutcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onBackButtonTap(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func onHomeButtonTap(sender: AnyObject) {
+        var storyboard = UIStoryboard (name: "Main", bundle: nil)
+        var controller: MilestonesViewController = storyboard.instantiateViewControllerWithIdentifier("MilestonesVCStoryboardID") as! MilestonesViewController
+        self.presentViewController(controller, animated: true, completion: nil);
+    }
+
+}
---
+++ questionApp/UnassistedSittingWhatWillYouNeedViewControllerswift.swift
@@ -0,0 +1,53 @@
+//
+//  PartiallyCoveredToyWhatWillYouNeedViewController.swift
+//  questionApp
+//
+//  Created by Daniel Hsu on 7/30/15.
+//  Copyright (c) 2015 codeHatcher. All rights reserved.
+//
+
+import UIKit
+
+class PartiallyCoveredToyWhatWillYouNeedViewController: UIViewController {
+
+    @IBOutlet weak var testPreparationLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        applyTextAttributesToLabel()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onNextStepButtonTap(sender: AnyObject) {
+        let dontShowIsBabyReadyVC = NSUserDefaults.standardUserDefaults().boolForKey("dontShowIsBabyReady")
+        if dontShowIsBabyReadyVC == true {
+            performSegueWithIdentifier("partiallyCoveredToyTimeToTestSegueID", sender: self)
+        } else {
+            performSegueWithIdentifier("partiallyCoveredToyIsBabyReadySegueID", sender: self)
+        }
+    }
+    
+    @IBAction func onBackButtonTap(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    // Helper function formats text attributes for multiple substrings in label.
+    func applyTextAttributesToLabel() {
+        
+        let string = "Any small, colorful toy will do, plus a cloth large enough to cover the toy."
+
+        var attributedString = NSMutableAttributedString(string: string)
+        
+        let baseAttributes = [NSForegroundColorAttributeName: kGrey, NSFontAttributeName: UIFont(name: kOmnesFontMedium, size: 22)!]
+        let firstAttributes = [NSForegroundColorAttributeName: kGrey, NSFontAttributeName: UIFont(name: kOmnesFontSemiBold, size: 22)!]
+        let secondAttributes = [NSForegroundColorAttributeName: kGrey, NSFontAttributeName: UIFont(name: kOmnesFontMedium, size: 22)!]
+        
+        attributedString.addAttributes(baseAttributes, range: NSMakeRange(0, 76))
+        
+        testPreparationLabel.attributedText = attributedString
+    }
+}
