b501ac1f377c0736e45ebcc071e9f368369cca46
Daniel Hsu <danieljhsu@gmail.com>
2015-08-07 23:15:56 -0700

    set up scaffolding for completely covered toy test #56

---
+++ questionApp/CompletelyCoveredToyActivityReminderViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PartiallyCoveredToyActivityReminderViewController.swift
+//  questionApp
+//
+//  Created by Daniel Hsu on 7/30/15.
+//  Copyright (c) 2015 codeHatcher. All rights reserved.
+//
+
+import UIKit
+
+class PartiallyCoveredToyActivityReminderViewController: ActivityReminderViewController {
+   
+    @IBAction func onHomeButtonTap(sender: AnyObject) {
+        var storyboard = UIStoryboard (name: "Main", bundle: nil)
+        var controller: MilestonesViewController = storyboard.instantiateViewControllerWithIdentifier("MilestonesVCStoryboardID") as! MilestonesViewController
+        self.presentViewController(controller, animated: true, completion: nil);
+    }
+}
