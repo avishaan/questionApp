e21fb97538a9192efed30ca737f6dcb9f2c3412e
jbateman <johnbateman00@gmail.com>
2015-08-12 12:14:50 -0700

    added remind me notification to partially covered toy test.

--- questionApp/PartiallyCoveredToyGoodOutcomeViewController.swift
+++ questionApp/PartiallyCoveredToyGoodOutcomeViewController.swift
@@ -12,6 +12,9 @@ class PartiallyCoveredToyGoodOutcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // If a reminder notification had previously been scheduled, remove it now that the test has been passed.
+        BNLocalNotification.removeLocalNotification(Test.TestNamesPresentable.partiallyCoveredToy)
     }
 
     override func didReceiveMemoryWarning() {
