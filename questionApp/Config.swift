85bad61919e21371e10039c271c757b7b527c931
Avishaan <avishaan@codeHatcher.com>
2015-08-17 02:20:53 -0700

    add global config file

---
+++ questionApp/Config.swift
@@ -0,0 +1,13 @@
+//
+//  Config.swift
+//  questionApp
+//
+//  Created by Brown Magic on 8/17/15.
+//  Copyright (c) 2015 codeHatcher. All rights reserved.
+//
+
+import Foundation
+
+struct config {
+  var mixpanel = (token: "1446f2fe2273c86febd26066953e49f1", apiKey: "1446f2fe2273c86febd26066953e49f1", apiSecret: "1446f2fe2273c86febd26066953e49f1")
+}
