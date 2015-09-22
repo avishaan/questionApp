b7a7bfdd07de193a61c25916175712b165f3c549
Lekshmi Raveendranath <lekshmi.ravindranath@gmail.com>
2015-09-21 17:49:09 -0700

    Emotional Security Test
    UI Design complete
    Started implementation

---
+++ questionApp/EmotionalSecurityActivityReminder.swift
@@ -0,0 +1,9 @@
+//
+//  EmotionalSecurityActivityReminder.swift
+//  questionApp
+//
+//  Created by Lekshmi on 9/21/15.
+//  Copyright (c) 2015 codeHatcher. All rights reserved.
+//
+
+import Foundation
