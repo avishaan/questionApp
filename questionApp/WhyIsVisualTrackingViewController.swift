b833cf7d4b8ed6d1f7c4fc60dcc4d6981566e344
Lekshmi Raveendranath <lekshmi.ravindranath@gmail.com>
2015-09-22 14:13:31 -0700

    Visual Tracking Test
    Completed UI Design

--- questionApp/WhyIsVisualTrackingViewController.swift
+++ questionApp/WhyIsVisualTrackingViewController.swift
@@ -10,26 +10,39 @@ import UIKit
 
 class WhyIsVisualTrackingViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  @IBOutlet weak var descriptionLabel: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // analytics
+    Tracker.createEvent(.RollingBackToFront, .Load, .Why)
+    applyTextAttributesToLabel()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  @IBAction func onBackButtonTap(sender: AnyObject) {
+    self.dismissViewControllerAnimated(true, completion: nil)
+  }
+  
+  // Helper function formats text attributes for multiple substrings in label.
+  func applyTextAttributesToLabel() {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    let string = "Rolling is indication of gross motor development. If there's developmental delays, therapy early on can help baby get back on track."
+    
+    var attributedString = NSMutableAttributedString(string: string)
+    
+    let boldAttributes = [NSForegroundColorAttributeName: kGrey, NSFontAttributeName: UIFont(name: kOmnesFontSemiBold, size: 22)!]
+    let standardAttributes = [NSForegroundColorAttributeName: kGrey, NSFontAttributeName: UIFont(name: kOmnesFontMedium, size: 22)!]
+    
+    attributedString.addAttributes(standardAttributes, range: NSMakeRange(0, 60))
+    attributedString.addAttributes(boldAttributes, range: NSMakeRange(61, 20))
+    //    attributedString.addAttributes(standardAttributes, range: NSMakeRange(71, 75))
+    
+    descriptionLabel.attributedText = attributedString
+  }
 
 }
