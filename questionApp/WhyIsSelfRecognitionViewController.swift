4d60ed83262fc72b4d5eda32a844f4d06e54b9b1
Avishaan <avishaan@codeHatcher.com>
2015-09-24 22:56:58 -0400

    update text close #227

--- questionApp/WhyIsSelfRecognitionViewController.swift
+++ questionApp/WhyIsSelfRecognitionViewController.swift
@@ -31,17 +31,17 @@ class WhyIsSelfRecognitionController: UIViewController {
     // Helper function formats text attributes for multiple substrings in label.
     func applyTextAttributesToLabel() {
         
-        let string = "This test assesses your baby’s ability to recognize her own reflection. This is one step in your baby’s development of visual recognition that begins with being able to identify a parent’s face."
+        let string = "This test assesses baby’s ability to recognize baby's own reflection. This is one step in baby’s development of visual recognition that begins with being able to identify a parent’s face."
         
         var attributedString = NSMutableAttributedString(string: string)
         
         let boldAttributes = [NSForegroundColorAttributeName: kGrey, NSFontAttributeName: UIFont(name: kOmnesFontSemiBold, size: 22)!]
         let standardAttributes = [NSForegroundColorAttributeName: kGrey, NSFontAttributeName: UIFont(name: kOmnesFontMedium, size: 22)!]
         
-        attributedString.addAttributes(standardAttributes, range: NSMakeRange(0, 54))
-        attributedString.addAttributes(boldAttributes, range: NSMakeRange(55, 14))
-        attributedString.addAttributes(standardAttributes, range: NSMakeRange(70, 105))
-        
+//        attributedString.addAttributes(standardAttributes, range: NSMakeRange(0, 54))
+//        attributedString.addAttributes(boldAttributes, range: NSMakeRange(55, 14))
+//        attributedString.addAttributes(standardAttributes, range: NSMakeRange(70, 105))
+      
         descriptionLabel.attributedText = attributedString
     }
 }
