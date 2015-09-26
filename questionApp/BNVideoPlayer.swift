eb9af2f3da27d291aff5b589a45f747214bfc225
Avishaan <avishaan@codeHatcher.com>
2015-09-25 23:06:07 -0400

    basic custom video controller setup

---
+++ questionApp/BNVideoPlayer.swift
@@ -0,0 +1,21 @@
+//
+//  BNVideoPlayer.swift
+//  questionApp
+//
+//  Created by Brown Magic on 9/25/15.
+//  Copyright (c) 2015 codeHatcher. All rights reserved.
+//
+
+import UIKit
+
+class BNVideoPlayer: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
