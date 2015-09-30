1eb2a76f026d393361e3fb720cd389944b18b1b3
Avishaan <avishaan@codeHatcher.com>
2015-09-30 14:48:21 -0700

    change preview video to xib fix #274

--- questionApp/SelfRecognitionTestOverviewViewController.swift
+++ questionApp/SelfRecognitionTestOverviewViewController.swift
@@ -12,9 +12,6 @@ import AVFoundation
 
 class SelfRecognitionTestOverviewViewController: UIViewController {
 
-    @IBOutlet weak var previewButton: UIButton!
-    var playerVC:AVPlayerViewController!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
       // analytics
@@ -23,7 +20,6 @@ class SelfRecognitionTestOverviewViewController: UIViewController {
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        enableVideoReplay()
     }
     
     override func viewWillDisappear(animated: Bool) {
@@ -38,48 +34,12 @@ class SelfRecognitionTestOverviewViewController: UIViewController {
     
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "symmetryEmbeddedVideoSegue" {
-            // set the playerVC as the destination
-            playerVC = segue.destinationViewController as! AVPlayerViewController
-            let path = NSBundle.mainBundle().pathForResource("self recognition", ofType: "mp4")
-            let url = NSURL.fileURLWithPath(path!)
-            // let url = NSURL(string: "crawl.mp4") // for remote locations
-            
-            // hide player controls
-            playerVC.showsPlaybackControls = false
-            playerVC.hidesBottomBarWhenPushed = true
-            playerVC.videoGravity = AVLayerVideoGravityResizeAspectFill
-            
-            playerVC.player = AVPlayer(URL: url)
-            // we start off paused, then we will play once the button is hit
-            playerVC.player?.pause()
-            
-            // listen for video end notification
-            NSNotificationCenter.defaultCenter().addObserver(self,
-                selector: "enableVideoReplay",
-                name: AVPlayerItemDidPlayToEndTimeNotification,
-                object: playerVC.player?.currentItem)
-        }
-        else if segue.identifier == "selfRecognitionWhatWillYouNeedSegueID" {
-            playerVC.player?.pause()
-        }
     }
     
     func enableVideoReplay() {
-        playerVC.player?.seekToTime(kCMTimeZero)
-        // show button
-        previewButton.hidden = false
     }
-    
-    @IBAction func onPreviewButtonTap(button: UIButton) {
-        // hide button
-        button.hidden = true
-        // play the video
-        playerVC.player?.play()
-    }
-    
+  
     @IBAction func onBackTap(sender: BNBackButton) {
-        playerVC.player?.pause()
         self.dismissViewControllerAnimated(true, completion: nil)
     }
 }
