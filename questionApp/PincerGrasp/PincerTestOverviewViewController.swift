9eb42d01257bd406ab3a68a20451daa031692035
Michael Lo <mmmilo@MacBook-Pro.local>
2015-09-30 18:06:02 -0400

    Removing unused older video code

--- questionApp/PincerGrasp/PincerTestOverviewViewController.swift
+++ questionApp/PincerGrasp/PincerTestOverviewViewController.swift
@@ -13,8 +13,7 @@ import AVFoundation
 class PincerTestOverviewViewController: UIViewController {
 
     @IBOutlet weak var previewButton: UIButton!
-    var playerVC:AVPlayerViewController!
-    
+  
     override func viewDidLoad() {
         super.viewDidLoad()
       
@@ -39,34 +38,13 @@ class PincerTestOverviewViewController: UIViewController {
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "pincerEmbeddedVideoSegue" {
-            // set the playerVC as the destination
-            playerVC = segue.destinationViewController as! AVPlayerViewController
-            let path = NSBundle.mainBundle().pathForResource("pincer grasp", ofType: "mp4")
-            let url = NSURL.fileURLWithPath(path!)
-            // let url = NSURL(string: "crawl.mp4") // for remote locations
-            
-            // hide player controls
-            playerVC.showsPlaybackControls = false
-            playerVC.hidesBottomBarWhenPushed = true
-            playerVC.videoGravity = AVLayerVideoGravityResizeAspectFill
-            
-            playerVC.player = AVPlayer(URL: url)
-            // we start off paused, then we will play once the button is hit
-            playerVC.player?.pause()
-            
-            // listen for video end notification
-            NSNotificationCenter.defaultCenter().addObserver(self,
-                selector: "enableVideoReplay",
-                name: AVPlayerItemDidPlayToEndTimeNotification,
-                object: playerVC.player?.currentItem)
+          
         }
         else if segue.identifier == "pincerWhatWillYouNeedSegueID" {
-            playerVC.player?.pause()
         }
     }
     
     @IBAction func onBackTap(sender: BNBackButton) {
-        playerVC.player?.pause()
         self.dismissViewControllerAnimated(true, completion: nil)
     }
 }
