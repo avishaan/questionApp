0d3b3b1475d1bf7e3306a8d5cbad9c9f2fdd6907
Avishaan <avishaan@codeHatcher.com>
2015-10-01 02:33:08 -0700

    fix image size for video, fix crashing when at "what do you need VC"

--- questionApp/EmotionalSecurityOverviewViewController.swift
+++ questionApp/EmotionalSecurityOverviewViewController.swift
@@ -11,8 +11,6 @@ import AVKit
 import AVFoundation
 
 class EmotionalSecurityOverviewViewController: UIViewController {
-  @IBOutlet weak var previewButton: UIButton!
-  var playerVC:AVPlayerViewController!
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -22,7 +20,6 @@ class EmotionalSecurityOverviewViewController: UIViewController {
   
   override func viewWillAppear(animated: Bool) {
     super.viewWillAppear(animated)
-    enableVideoReplay()
   }
   
   override func viewWillDisappear(animated: Bool) {
@@ -37,49 +34,12 @@ class EmotionalSecurityOverviewViewController: UIViewController {
   
   
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    if segue.identifier == "EmotionalSecurityEmbeddedVideoSegue"
-    {
-      // set the playerVC as the destination
-      playerVC = segue.destinationViewController as! AVPlayerViewController
-      let path = NSBundle.mainBundle().pathForResource("emotional security", ofType: "mp4")
-      let url = NSURL.fileURLWithPath(path!)
-      // let url = NSURL(string: "crawl.mp4") // for remote locations
-      
-      // hide player controls
-      playerVC.showsPlaybackControls = false
-      playerVC.hidesBottomBarWhenPushed = true
-      playerVC.videoGravity = AVLayerVideoGravityResizeAspectFill
-      
-      playerVC.player = AVPlayer(URL: url)
-      // we start off paused, then we will play once the button is hit
-      playerVC.player?.pause()
-      
-      // listen for video end notification
-      NSNotificationCenter.defaultCenter().addObserver(self,
-        selector: "enableVideoReplay",
-        name: AVPlayerItemDidPlayToEndTimeNotification,
-        object: playerVC.player?.currentItem)
-    }
-    else if segue.identifier == "EmotionalSecurityWhatWillYouNeedSegueID" {
-      playerVC.player?.pause()
-    }
   }
   
   func enableVideoReplay() {
-    playerVC.player?.seekToTime(kCMTimeZero)
-    // show button
-    previewButton.hidden = false
-  }
-  
-  @IBAction func onPreviewButtonTap(button: UIButton) {
-    // hide button
-    button.hidden = true
-    // play the video
-    playerVC.player?.play()
   }
   
   @IBAction func onBackTap(sender: BNBackButton) {
-    playerVC.player?.pause()
     self.dismissViewControllerAnimated(true, completion: nil)
   }
 
